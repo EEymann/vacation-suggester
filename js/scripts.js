bba2391ea15384fee0995fe5f3a83bc758737c12
Elizabeth Eymann <elizabeth.eymann@gmail.com>
2019-06-08 23:10:18 -0700

    fix business logic and add css styling

--- js/scripts.js
+++ js/scripts.js
@@ -6,15 +6,21 @@ $(document).ready(function () {
   });
 
   $("form#match").submit(function (event) {
+    event.preventDefault();
     var userName = $("input#userName").val();
     var age = $("select#age").val();
     var gender = $("select#gender").val();
     var environment = $("select#environment").val();
     var location = $("select#location").val();
     var energy = $("select#energy").val();
+    
+    console.log(userName);
+    $(".name").text(userName);
 
 
-      if ((age >= '18-25') && gender === 'female' || 'male' || 'non-binary' && environment === 'tropical' && location === 'america') {
+
+
+      if ((age >= '18-25') && (gender === 'female' || 'male' || 'non-binary') && environment === 'tropical' && location === 'america') {
         $("#hawaii").fadeIn();
         $("#arizona").hide();
         $("#ibiza").hide();
@@ -24,18 +30,18 @@ $(document).ready(function () {
         $("#arizona").hide();
         $("#ibiza").fadeIn();
         $("#switzerland").hide();
-      } else if ((age >= '18-25') && (gender === 'female' || 'male' || 'non-binary') && (environment === 'tropical') && location === 'america') {
+      } else if ((age >= '18-25') && (gender === 'female' || 'male' || 'non-binary') && (environment === 'dry') && location === 'america') {
         $("#hawaii").hide();
         $("#arizona").fadeIn();
         $("#ibiza").hide();
         $("#switzerland").hide();
-      } else if ((age >= '18-25') && (gender === 'female' || 'male' || 'non-binary') && (environment === 'tropical') && location === 'anywhere') {
+      } else if ((age >= '18-25') && (gender === 'female' || 'male' || 'non-binary') && (environment === 'dry') && location === 'anywhere') {
         $("#hawaii").hide();
         $("#arizona").hide();
         $("#ibiza").hide();
         $("#switzerland").fadeIn();
       }
   
-    event.preventDefault();
+
   });
 });
