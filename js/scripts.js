4b65926be0b8bc687e5ddcca323d4a703db8f53c
Elizabeth Eymann <elizabeth.eymann@gmail.com>
2019-06-08 17:23:21 -0700

    attempt to fix business and user logic in js

--- js/scripts.js
+++ js/scripts.js
@@ -1,29 +1,41 @@
 $(document).ready(function () {
   $("form#match").submit(function (event) {
-    event.preventDefault();
-    var quote = '';
-    var name = parseInt($("input#name").val());
+    
+    var userName = $("input#userName").val();
+    $(".userName").text(userName);
+  });
+
+  $("form#match").submit(function (event) {
+    var userName = $("input#userName").val();
     var age = $("select#age").val();
     var gender = $("select#gender").val();
-    var climate = $("select#climate").val();
+    var environment = $("select#environment").val();
     var location = $("select#location").val();
     var energy = $("select#energy").val();
 
-    if (age >= '18-25') {
-      if (gender === 'female' || 'male' || 'non-binary') {
-        if (climate === 'tropical' && location === 'America') {
-          console.log('this happens, 1');
-          quote = "Hawaii";
-        }
-        if (climate === 'dry' && location === 'America') {
-          console.log('that happens, 2');
-          quote = "Arizona";
-        }
+
+      if ((age >= '18-25') && gender === 'female' || 'male' || 'non-binary' && environment === 'tropical' && location === 'america') {
+        $("#hawaii").fadeIn();
+        $("#arizona").hide();
+        $("#ibiza").hide();
+        $("#switzerland").hide();
+      } else if ((age >= '18-25') && (gender === 'female' || 'male' || 'non-binary') && (environment === 'tropical') && location === 'anywhere') {
+        $("#hawaii").hide();
+        $("#arizona").hide();
+        $("#ibiza").fadeIn();
+        $("#switzerland").hide();
+      } else if ((age >= '18-25') && (gender === 'female' || 'male' || 'non-binary') && (environment === 'tropical') && location === 'america') {
+        $("#hawaii").hide();
+        $("#arizona").fadeIn();
+        $("#ibiza").hide();
+        $("#switzerland").hide();
+      } else if ((age >= '18-25') && (gender === 'female' || 'male' || 'non-binary') && (environment === 'tropical') && location === 'anywhere') {
+        $("#hawaii").hide();
+        $("#arizona").hide();
+        $("#ibiza").hide();
+        $("#switzerland").fadeIn();
       }
-      $("#vacation-match").empty().append(quote);
-      $("#quote").show();
-    } else {
-      alert('Sorry, you are too young to choose your vacation. Please ask a parent or guardian to help.');
-    }
+  
+    event.preventDefault();
   });
 });
